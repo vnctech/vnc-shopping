efcd571251b9a5fb7bc81fb1afede3bb7e950a6b
mdmubi789 <mdmubi789@gmail.com>
2024-09-24 20:23:59 +0530

    I Setup product management app

---
+++ mihman/apps/product_management/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Product
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'product_management/product_list.html', {'products': products})
+
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+    return render(request, 'product_management/product_detail.html', {'product': product})
